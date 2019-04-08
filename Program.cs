4db93b5c91a38ead4772a50bb5fda543016084b6
Panya Farnette <naienko@gmail.com>
2019-04-08 14:12:08 -0500

    completed lightning exercise #2

--- Program.cs
+++ Program.cs
@@ -5,23 +5,27 @@ namespace dictionaries
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-			Dictionary<string, List<string>> idioms = new Dictionary<string, List<string>>();
-			idioms.Add("Penny", new List<string> { "A", "penny", "for", "your", "thoughts" });
-			idioms.Add("Injury", new List<string> { "Add", "insult", "to", "injury" });
-			idioms.Add("Moon", new List<string> { "Once", "in", "a", "blue", "moon" });
-			idioms.Add("Grape", new List<string> { "I", "heard", "it", "through", "the", "grapevine" });
-			idioms.Add("Murder", new List<string> { "Kill", "two", "birds", "with", "one", "stone" });
-			idioms.Add("Limbs", new List<string> { "It", "costs", "an", "arm", "and", "a", "leg" });
-			idioms.Add("Grain", new List<string> { "Take","what","someone","says","with","a","grain","of","salt" });
-			idioms.Add("Fences", new List<string> { "I'm", "on", "the", "fence", "about", "it" });
-			idioms.Add("Sheep", new List<string> { "Pulled", "the", "wool", "over", "his", "eyes" });
-			idioms.Add("Lucifer", new List<string> { "Speak", "of", "the", "devil" });
+// create a dictionary with key value pairs to represent words (key) and its definition (value) 
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
 
-			foreach (KeyValuePair<string, List<string>> item in idioms) {
-				Console.WriteLine($"{item.Key}: {String.Join(" ", item.Value)}");		
-			}
+            // add several words and their definition
+            wordsAndDefinitions.Add("Cohort 30", "Pretty damn cool cohort");
+            wordsAndDefinitions.Add("Awesome", "Cohort 30 learning C#");
+			wordsAndDefinitions.Add("Sexy", "Grant Gustin");
+			wordsAndDefinitions.Add("Glasses", "Annoying");
+
+            // use square bracket lookup to get the definition of a specific word
+
+			Console.WriteLine($"Sexy is {wordsAndDefinitions["Sexy"]}");
+
+            // loop over dictionary to get the following output: "The definition of [WORD] is [DEFINITION]"
+            foreach (KeyValuePair<string, string> word in wordsAndDefinitions) {
+				Console.WriteLine($"The definition of {word.Key} is {word.Value}.");
+            }
+
         }
     }
 }
+
